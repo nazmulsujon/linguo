09ac3e77a267f60e47deb2a4decfb59e70258b8c
Nazmul Hoque <sujon686500@gmail.com>
2024-09-13 14:14:23 +0600

    deploy issue fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  build: {
+    outDir: "dist", // Explicitly set the output directory
+  },
 });
